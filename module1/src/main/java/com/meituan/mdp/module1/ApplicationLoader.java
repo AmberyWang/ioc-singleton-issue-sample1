43fb3e55930efe8525ed2bd336a4b2804768a4ad
wangtingting28 <wangtingting28@meituan.com>
2022-07-03 16:37:49 +0800

    first commit

---
+++ module1/src/main/java/com/meituan/mdp/module1/ApplicationLoader.java
@@ -0,0 +1,22 @@
+package com.meituan.mdp.module1;
+
+import com.meituan.mdp.module2.EnglishCat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@Import(EnglishCat.class)
+@ImportResource({"classpath:spring/applicationContext.xml"})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class ApplicationLoader {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(ApplicationLoader.class);
+        application.setAdditionalProfiles("test");
+        application.run(args);
+    }
+}
+
+
